de4d0eb1ed6e20d9739e4d9879a8274700896916
itdanjalog <kgs2072@naver.com>
2025-01-15 13:21:35 +0900

    day01

--- tj2024b_web1/src/main/java/day01/Example3.java
+++ tj2024b_web1/src/main/java/day01/Example3.java
@@ -3,16 +3,16 @@ package day01;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//@WebServlet("http://192.168.40.25:8080/tj2024b_web1/day01/example1") : URL : 절대경로
-@WebServlet("/day01/example1") // : URL : 상대경로(프로젝트명 생략 가능)
-public class Example1 extends HttpServlet  {
+public class Example3 extends HttpServlet {
+	
+	// doget + 자동완성 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("안녕 나는 서블릿이야.");
+		System.out.println("안녕 나는 서블릿이야3");
 	}
-}
+
+} // class end 
