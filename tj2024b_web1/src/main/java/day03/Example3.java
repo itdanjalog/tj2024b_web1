1f1189d3efe08abb89021280f5f9a16c8c73f063
itdanjalog <kgs2072@naver.com>
2025-01-20 11:56:03 +0900

    day03

--- tj2024b_web1/src/main/java/day03/Example3.java
+++ tj2024b_web1/src/main/java/day03/Example3.java
@@ -1,6 +1,9 @@
 package day03;
 
 import java.io.IOException;
+import java.io.Reader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -16,14 +19,22 @@ public class Example3 extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// * JOSN자료의 문자열타입을 DTO로 변환해보기. ( JSON는 JS언어에서 사용하는 타입 , JAVA는 DTO 사용한다. )
 		System.out.println( "[HTTP *POST 방식으로 요청이 왔어요.]");
-		System.out.println( req.getReader().readLine() ); // 단순한 문자열 읽어왔다.
+		// System.out.println( req.getReader().readLine() ); // 단순한 문자열 읽어왔다.
 		// 문자열 : { "data1" : "유재석" , "data2" : "40"  } --> DTO 변환 
-		// JSON 모양의 문자열타입 자료를 DTO 로 변환 라이브러리가 필요하다.
+		// JSON 모양의 문자열타입 자료를 DTO 로 변환 라이브러리가 필요하다. , jackson 3종 라이브러리 등록 
+		ObjectMapper mapper = new ObjectMapper(); // ObjectMapper 인스턴스 생성 
+		// req.getReader() 객체의 자료(JSON)들을 DTO 객체 로 변환 함수.
+		// .readValue( JSON데이터자료 , 변환할객체타입.class );
+		DataDto dataDto = mapper.readValue( req.getReader() , DataDto.class  ); 
+		System.out.println( dataDto );
+		// DTO : DataDto [data1=유재석, data2=40]
 	} // f end 
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println( "[HTTP *PUT 방식으로 요청이 왔어요.]");
-		System.out.println( req.getReader().readLine() );
+		ObjectMapper mapper = new ObjectMapper();
+		DataDto dataDto = mapper.readValue( req.getReader() , DataDto.class );
+		System.out.println( dataDto );
 	} // f end 
 	
 } // class end 
