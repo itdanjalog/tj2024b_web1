6b9ba1bf8956862f537a21e7daea72983c7c5f8f
itdanjalog <kgs2072@naver.com>
2025-01-21 13:23:23 +0900

    day04

--- tj2024b_web1/src/main/webapp/day04/example2.js
+++ tj2024b_web1/src/main/webapp/day04/example2.js
@@ -48,25 +48,100 @@ const func9 = ( ) => {
 	fetch( `/tj2024b_web1/day02/example2?name=${ name }&age=${ age }` , option )
 } // f end 
 
+const func10 =  ( ) => {
+	let object = { data1 : '유재석' , data2 : 50 }
+	const option = { 
+		method : 'POST' ,
+		headers : { 'Content-Type' : 'application/json' } ,
+		body : JSON.stringify( object ) // HTTP 통신은 문자열 자료만 전송이 가능하다.
+	 } // o end 
+	fetch( `/tj2024b_web1/day03/example3` , option );
+} // f end 
+
+const func11 = ( ) => {
+	let object = { data1 : '강호동' , data2 : 40 }
+	const option = {
+		method : 'PUT',
+		headers : { 'Content-Type' : 'application/json'} , 
+		body : JSON.stringify( object )
+	} // o end 
+	fetch( `/tj2024b_web1/day03/example3` , option );
+}// f ned 
+
+const func12 = ( ) => {
+	const option = { method : 'GET'}
+	fetch( `/tj2024b_web1/day03/example5` , option )
+		.then( response => response.json() )
+		.then( data => { console.log( data ); })
+} // f end 
+
+const func13 = ( ) => {
+	const option = { method : 'POST' }
+	fetch( `/tj2024b_web1/day03/example5` , option )
+		.then( response => response.text() )
+		.then( data => { console.log( data ); } )
+} // f end 
+
+const func14 = ( ) => {
+	const option = { method : 'PUT' }
+	fetch( `/tj2024b_web1/day03/example5` , option )
+		.then( response => response.json() )		// 통신 성공시 
+		.then( data => { console.log( data ); } )	
+		.catch( error => { console.log( error); } )	// 통신 실패시 
+} // f end 
+
+const func15 = ( ) => {
+	const option = { method : 'DELETE' }
+	fetch( `/tj2024b_web1/day03/example5` , option )
+		.then( r => r.json() )
+		.then( data => { console.log(data ); } )
+		.catch( e => { console.log(e); } )
+}
+
+
+
 
 
 /*
 	fetch( `HTTP URL` , {옵션} )
+		.then( response객체 => response.타입() )
+		.then( 타입변환된자료 => { 실행문 } )
+		
+	
 		URL 
 			1. 통신할 서블릿의 URL 주소 
 			2. 쿼리스트링 
 		옵션
 			1. METHOD 
-			- GET : fetch( `HTTP URL` , { method: `GET` } )]
-				-> GET METHOD는 기본값이므로 생략이 가능하다.
-				
-			- POST : fetch( `HTTP URL` , { method: `POST` } )
-			- PUT : fetch( `HTTP URL` , { method: `PUT` } )
-			- DELETE : fetch( `HTTP URL` , { method: `DELETE` } )
+				- GET : fetch( `HTTP URL` , { method: `GET` } )]
+					-> GET METHOD는 기본값이므로 생략이 가능하다.
+					
+				- POST : fetch( `HTTP URL` , { method: `POST` } )
+				- PUT : fetch( `HTTP URL` , { method: `PUT` } )
+				- DELETE : fetch( `HTTP URL` , { method: `DELETE` } )
+			2. HEADERS
+				- { 'Content-Type' : 'application/json' }
+			3. BODY : 
+				- 전송할 데이터 자료 
+		then( )
+			1. 응답객체 : 통신한 응답의 정보가 담긴 HTTP 응답 객체 반환 
+				then( 응답객체명 => 응답객체명.json() ) : response content-type : application/json 변환
+				then( 응답객체명 => 응답객체명.text() ) : response content-type : text/plain 변환
+		then( )
+			1. 변환된 자료 객체 		
+		catch( )
+			
   	
+	[참고1 : 백틱]
 	`` : 백틱 템플릿 : 문자열 사이에 변수/함수 호출 과 연산식을 넣을수 있는 템플릿 
 		`문자열 ${ 변수명 } 문자열 ${ 함수명() } 문자열 ${ 연산 } `
+	
+	[참고2 : JSON 문자열 타입변환 ]
+		1. JSON.parse( )		: 문자열타입 --> JSON타입 변환 함수. 
+		2. JSON.stringify( )	: JSON타입 --> 문자열타입 변환 함수.
+		
 */
+
 
 
 
--- tj2024b_web1/src/main/webapp/day04/example2.jsp
+++ tj2024b_web1/src/main/webapp/day04/example2.jsp
@@ -18,11 +18,23 @@
 		<button onclick="func4()"> func4(PUT) </button>
 		<button onclick="func5()"> func5(DELETE) </button>
 	
-		<h5> [3] Fetch 함수 이용한 HTTP queryString </h5>
+		<h5> [3] Fetch 함수 이용한 HTTP queryString 요청 통신 </h5>
 		<button onclick="func6()"> func6(GET) </button>
 		<button onclick="func7()"> func7(POST) </button>
 		<button onclick="func8()"> func8(PUT)</button>
 		<button onclick="func9()"> func9(DELETE)</button>
+		
+		<h5> [4] Fetch 함수 이용한 HTTP HEADER BODY 요청 통신 </h5>
+		<button onclick="func10()"> func10(POST) </button>
+		<button onclick="func11()"> func11(PUT) </button>
+		<span> GET / DELETE method는 BODY 비권장 </span>
+		
+		<h5> [5] Fetch 함수 이용한 HTTP HEADER BODY 응답 통신 </h5>
+		<button onclick="func12()"> func12(GET) </button>
+		<button onclick="func13()"> func13(POST) </button>
+		<button onclick="func14()"> func14(PUT) </button>
+		<button onclick="func15()"> func15(DELETE) </button>
+		
 	</div>
 	
 
